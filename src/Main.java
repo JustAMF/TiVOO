82517982e362c3913237121d49c677f7ff4bed43
Gang Song <gang.song@duke.edu>
2012-03-04 17:09:43 -0500

    Modified main class

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,6 @@
-import html_output.ConflictingEventsPage;
-import html_output.DayCalendarPage;
-import html_output.DetailPage;
-import html_output.MonthCalendarPage;
-import html_output.SortedEventsPage;
-import html_output.SummaryPage;
-import html_output.WeekCalendarPage;
-
-
-import org.joda.time.DateTime;
 
 import controller.TivooSystem;
 
-import process.EventCalendar;
 import view.TiVOOViewer;
 
 public class Main {
